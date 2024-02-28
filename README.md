a755edd92673f6c311ba4b7c35473b89e22046ca
Mikael Morlund <mikael.morlund@gmail.com>
2024-02-28 15:11:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite + Tailwind
+
+http://localhost:3042/
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-2xl font-bol text-blue-500">
+      React Vite Tailwind
+    </h1>
+  )
+}
